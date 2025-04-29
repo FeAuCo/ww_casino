a1f324ee2bfa9a3078b34de9ed31190b8f1db25d
FeAuCo <vladikyudin09@gmail.com>
2025-04-30 00:34:52 +0300

    adds users to db now

--- bot_related/handlers/command.py
+++ bot_related/handlers/command.py
@@ -1,7 +1,15 @@
+import asyncio
+import json
 import aiogram
 import ww_casino.bot_related.useful.keyboards as keyboards
 from aiogram.filters import CommandStart
+import os
+import requests
+import psycopg2 as pg
+import dotenv
+import datetime
 
+dotenv.load_dotenv()
 
 router = aiogram.Router()
 
@@ -9,3 +17,54 @@ router = aiogram.Router()
 @router.message(CommandStart())
 async def start_message(msg: aiogram.types.Message):
     await msg.answer('ДЕПАЕМ???', reply_markup=keyboards.modes_inline_keyboard)
+
+
+@router.message()
+async def add_message(msg: aiogram.types.Message):
+    if msg.text[:3] == 'add' and len(msg.text) > 3 and msg.text[3:].strip().isdigit():
+        invoice = requests.post(url='https://pay.xrocket.tg/tg-invoices',
+                                headers={'Rocket-Pay-Key': os.getenv('XROCKET_TOKEN')},
+                                json={
+                                    'amount': msg.text[3:],
+                                    'currency': "USDT",
+                                    'description': "ПОПОЛНЯЕМ",
+                                    'hiddenMessage': "СПАСИБО ЗА ПОПОЛНЕНИЕ БРАТ",
+                                    'commentsEnabled': False,
+                                    'expiredIn': 20
+                                }).json()
+
+        await msg.answer(invoice['data']['link'])
+
+        while True:
+            status = requests.get(url=f"https://pay.xrocket.tg/tg-invoices/{invoice['data']['id']}",
+                                  headers={'Rocket-Pay-Key': os.getenv('XROCKET_TOKEN')}).json()['data']['status']
+
+            if status == 'expired':
+                break
+
+            elif status == 'paid':
+                connection = pg.connect(user='postgres', dbname='postgres', host='localhost',
+                                        port=5432, password=os.getenv('POSTGRES_PASSWORD'))
+                cursor = connection.cursor()
+
+                cursor.execute(f'SELECT NOT EXISTS (SELECT {msg.chat.id} FROM "user")')
+
+                if cursor.fetchone()[0]:
+                    cursor.execute(
+                        f'INSERT INTO "user" (chat_id, registration_date, balance) VALUES {msg.chat.id, datetime.datetime.today().strftime("%Y-%m-%d"), json.dumps({"USDT": int(msg.text[3:])})}')
+                else:
+                    cursor.execute("SELECT balance -> 'USDT' AS " + '"USDT" FROM "user"')
+
+                    current_balance = cursor.fetchone()[0]
+
+                    cursor.execute(
+                        'UPDATE "user" SET balance = ' + f'jsonb_set(balance::jsonb, ' + "'{USDT}', " + f"'{int(msg.text[3:]) + current_balance}') " + f'WHERE "chat_id" = {msg.chat.id}')
+
+                connection.commit()
+
+                cursor.close()
+                connection.close()
+
+                break
+
+        await asyncio.sleep(1)
--- bot_related/handlers/message.py
+++ bot_related/handlers/message.py
@@ -1,21 +1,30 @@
 import aiogram
-import requests
-import os
 import dotenv
 
 dotenv.load_dotenv()
+
 router = aiogram.Router()
 
 
 @router.callback_query(aiogram.F.data == 'mode1')
 async def modes_reply(callback: aiogram.types.CallbackQuery):
-    await callback.message.answer(requests.post(url='https://pay.xrocket.tg/tg-invoices',
-                                                headers={'Rocket-Pay-Key': os.getenv('XROCKET_TOKEN')},
-                                                json={
-                                                    'amount': 500,
-                                                    'currency': "USDT",
-                                                    'description': "ДЕПАЕМ БРАТ",
-                                                    'hiddenMessage': "СПАСИБО ЗА ДЕП БРАТ",
-                                                    'commentsEnabled': False,
-                                                    'expiredIn': 20
-                                                }).json()['data']['link'])
+    await callback.message.answer("Сколько хотите депнуть?")
+    await callback.message.delete()
+
+
+@router.callback_query(aiogram.F.data == 'mode2')
+async def modes_reply(callback: aiogram.types.CallbackQuery):
+    await callback.message.answer("Сколько хотите депнуть?")
+    await callback.message.delete()
+
+
+@router.callback_query(aiogram.F.data == 'mode3')
+async def modes_reply(callback: aiogram.types.CallbackQuery):
+    await callback.message.answer("Сколько хотите депнуть?")
+    await callback.message.delete()
+
+
+@router.callback_query(aiogram.F.data == 'mode4')
+async def modes_reply(callback: aiogram.types.CallbackQuery):
+    await callback.message.answer("Сколько хотите депнуть?")
+    await callback.message.delete()
