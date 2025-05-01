c2920d44743ecfb3c9f32d0783e3f47605daba60
FeAuCo <vladikyudin09@gmail.com>
2025-05-01 17:53:39 +0300

    all currencies are available now

--- bot_related/handlers/command.py
+++ bot_related/handlers/command.py
@@ -5,9 +5,11 @@ import ww_casino.bot_related.useful.keyboards as keyboards
 from aiogram.filters import CommandStart
 import os
 import requests
-import psycopg2 as pg
 import dotenv
 import datetime
+import ww_casino.bot_related.useful.misc as misc
+import ww_casino.db as db
+import decimal
 
 dotenv.load_dotenv()
 
@@ -21,50 +23,74 @@ async def start_message(msg: aiogram.types.Message):
 
 @router.message()
 async def add_message(msg: aiogram.types.Message):
-    if msg.text[:3] == 'add' and len(msg.text) > 3 and msg.text[3:].strip().isdigit():
-        invoice = requests.post(url='https://pay.xrocket.tg/tg-invoices',
-                                headers={'Rocket-Pay-Key': os.getenv('XROCKET_TOKEN')},
-                                json={
-                                    'amount': msg.text[3:],
-                                    'currency': "USDT",
-                                    'description': "ПОПОЛНЯЕМ",
-                                    'hiddenMessage': "СПАСИБО ЗА ПОПОЛНЕНИЕ БРАТ",
-                                    'commentsEnabled': False,
-                                    'expiredIn': 20
-                                }).json()
+    if len(msg.text) > 3:
+        parameters = msg.text[3:].strip()
+        currency = []
 
-        await msg.answer(invoice['data']['link'])
-
-        while True:
-            status = requests.get(url=f"https://pay.xrocket.tg/tg-invoices/{invoice['data']['id']}",
-                                  headers={'Rocket-Pay-Key': os.getenv('XROCKET_TOKEN')}).json()['data']['status']
-
-            if status == 'expired':
+        for char in parameters:
+            if not char.isalpha():
                 break
 
-            elif status == 'paid':
-                connection = pg.connect(user='postgres', dbname='postgres', host='localhost',
-                                        port=5432, password=os.getenv('POSTGRES_PASSWORD'))
-                cursor = connection.cursor()
+            currency.append(char)
 
-                cursor.execute(f'SELECT NOT EXISTS (SELECT {msg.chat.id} FROM "user")')
+        currency = ''.join(currency)
+        amount = ''.join([digit for digit in parameters[len(currency):] if digit != ' '])
 
-                if cursor.fetchone()[0]:
-                    cursor.execute(
-                        f'INSERT INTO "user" (chat_id, registration_date, balance) VALUES {msg.chat.id, datetime.datetime.today().strftime("%Y-%m-%d"), json.dumps({"USDT": int(msg.text[3:])})}')
+        try:
+            amount = float(amount)
+            amount = decimal.Decimal(str(amount))
+
+            if currency in misc.currencies:
+
+                if msg.text[:3] == 'add':
+                    invoice = requests.post(url='https://pay.xrocket.tg/tg-invoices',
+                                            headers={'Rocket-Pay-Key': os.getenv('XROCKET_TOKEN')},
+                                            json={
+                                                'amount': float(json.dumps(decimal.Decimal(str(amount)), ensure_ascii=False, default=str)[1:-1]),
+                                                'currency': currency,
+                                                'description': "ПОПОЛНЯЕМ?",
+                                                'hiddenMessage': "СПАСИБО ЗА ПОПОЛНЕНИЕ БРАТ",
+                                                'commentsEnabled': False,
+                                                'expiredIn': 20
+                                            }).json()
+                    try:
+                        await msg.answer(invoice['data']['link'])
+
+                        while True:
+                            status = requests.get(url=f"https://pay.xrocket.tg/tg-invoices/{invoice['data']['id']}",
+                                                  headers={'Rocket-Pay-Key': os.getenv('XROCKET_TOKEN')}).json()['data']['status']
+
+                            if status == 'expired':
+                                break
+
+                            elif status == 'paid':
+                                user_db = db.DB()
+
+                                if user_db.if_registered_check(msg.chat.id):
+                                    user_db.add_user(msg.chat.id, datetime.datetime.today().strftime("%Y-%m-%d"))
+                                user_db.update_user_balance(msg.chat.id, currency, amount)
+
+                                user_db.commit()
+
+                                user_db.close_cursor_and_connection()
+
+                                del user_db
+
+                                break
+
+                            await asyncio.sleep(1)
+
+                    except KeyError:
+                        await msg.answer(text='Количество валюты неприемлемо')
+
                 else:
-                    cursor.execute("SELECT balance -> 'USDT' AS " + '"USDT" FROM "user"')
+                    await msg.answer(text='Команда введена неверно')
 
-                    current_balance = cursor.fetchone()[0]
+            else:
+                await msg.answer(text='Мы не предоставляем оплату этой валютой. Возможно вы ввели название валюты неверно')
 
-                    cursor.execute(
-                        'UPDATE "user" SET balance = ' + f'jsonb_set(balance::jsonb, ' + "'{USDT}', " + f"'{int(msg.text[3:]) + current_balance}') " + f'WHERE "chat_id" = {msg.chat.id}')
+        except ValueError:
+            await msg.answer(text='Вы ввели некорректную или равную нулю сумму')
 
-                connection.commit()
-
-                cursor.close()
-                connection.close()
-
-                break
-
-        await asyncio.sleep(1)
+    else:
+        await msg.answer(text='Команда введена неверно')
---
+++ bot_related/useful/misc.py
@@ -0,0 +1,7 @@
+currencies = ['TONCOIN', 'XROCK', 'SCALE', 'BOLT', 'TAKE', 'TNX', 'GRBS', 'JBCT', 'IVS', 'BTC', 'ANON', 'ATL', 'NUDES',
+              'WIF', 'MARGA', 'DUREV', 'SOX', 'UNIC', 'VIRUS1', 'ICTN', 'JMT', 'FID', 'CATS', 'WALL', 'NOT', 'OPEN',
+              'DHD', 'KINGY', 'GGT', 'JETTON', 'BNB', 'USDT', 'LIFEYT', 'GEMSTON', 'TRX', 'PUNK', 'TONNEL', 'DFC',
+              'ETH', 'ARBUZ', 'RAFF', 'DRIFT', 'FISH', 'MEOW', 'TINU', 'GRAM', 'WEB3', 'MRDN', 'LKY', 'STBL', '1RUSD',
+              'JVT', 'DRA', 'STATHAM', 'PLANKTON', 'VWS', 'SAU', 'CAVI', 'ALENKA', 'TIME', 'CES', 'KKX', 'HYDRA',
+              'GRC', 'tsTON', 'STON', 'DOGS', 'SOL', 'THNG', 'SP', 'AQUAXP', 'CATI', 'HMSTR', 'STORM', 'SPN', 'JETTY',
+              'MAJOR', 'FTON', 'CATSTG', 'BUILD', 'TRUMP', 'MELANIA', 'USDC', 'WOOF']
